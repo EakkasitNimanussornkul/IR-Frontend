1538703fa29193ddcb038f96a2f0a04b56d39b11
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2026-03-29 15:12:44 +0700

    added bookmark card type to index for bookmark card

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,6 +6,16 @@ export interface RecipeCard {
   score?: number
 }
 
+export interface BookmarkCard {
+  recipe_id: number
+  name: string
+  description_preview: string
+  image_url?: string
+  folder_id: number
+  rating?: number
+  score?: number
+}
+
 export interface Folder {
   id: number
   name: string
